a8ba3317470541875939f9911e75bf22e7fa14d7
Haiya-P <haiya_patel@yahoo.com>
2025-03-01 11:30:37 -0500

    Added imports for the extractor portion

--- api_extractor.py
+++ api_extractor.py
@@ -1,1 +1,9 @@
+from dataclasses import dataclass
+import logging
+import os
+import requests
+from datetime import datetime
 
+#logger goes here
+#os.environ goes here
+#I'll set up the logger and os.environ after we pick the map API- Haiya
