a1b01769135530a496c27d31d1f4b2ca41057f2e
Haiya-P <haiya_patel@yahoo.com>
2025-03-01 16:58:10 -0500

    Created a base code to show the first step in the route.

--- google_api.py
+++ google_api.py
@@ -1,1 +1,20 @@
+import googlemaps
+from datetime import datetime
 
+gmaps = googlemaps.Client(key='insert_api_key_here')
+
+now = datetime.now()
+
+address = "Smith Hall, 18 Amstel Ave, Newark, DE 19716"
+geocode_result = gmaps.geocode(address)
+
+print(f"Geocode result for {address}:")
+print(geocode_result)
+
+origin = "New York, NY"
+destination = "Delaware, DE"
+directions_result = gmaps.directions(origin, destination, departure_time=now)
+
+if directions_result:
+    print("\nFirst step of the route:")
+    print(directions_result[0]['legs'][0]['steps'][0]['html_instructions'])
