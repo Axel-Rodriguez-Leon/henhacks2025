6e475336b3a7f38989d3de11ee6efc50327e8a8d
Haiya-P <haiya_patel@yahoo.com>
2025-03-01 19:47:08 -0500

    Added functions to grab the user location and get directions to the location. User can put in the location address and get a google link to the route.

--- google_api.py
+++ google_api.py
@@ -1,20 +1,41 @@
 import googlemaps
+import requests
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='insert_api_key_here')
+gmaps = googlemaps.Client(key='api_key_here')
 
+#Getting the current time
 now = datetime.now()
 
-address = "Smith Hall, 18 Amstel Ave, Newark, DE 19716"
-geocode_result = gmaps.geocode(address)
+#Grabs the user's location, returns None if it fails
+def grab_user_location():
+    try:
+        response = requests.get("https://ipinfo.io/json")
+        data = response.json()
+        if "loc" in data:
+            lat, lng = map(float, data["loc"].split(","))
+            return f"{lat},{lng}" 
+    except Exception as e:
+        print("Error fetching location:", e)
+    return None
 
-print(f"Geocode result for {address}:")
-print(geocode_result)
+#Gets the directions to a location, and returns the URL to the route
+def get_directions_to_location(origin, destination):
+    now = datetime.now()
 
-origin = "New York, NY"
-destination = "Delaware, DE"
-directions_result = gmaps.directions(origin, destination, departure_time=now)
+    directions_result = gmaps.directions(origin, destination, departure_time=now)
 
-if directions_result:
-    print("\nFirst step of the route:")
-    print(directions_result[0]['legs'][0]['steps'][0]['html_instructions'])
+    if directions_result:
+        route_url = f"https://www.google.com/maps/dir/?api=1&origin={origin}&destination={destination}&travelmode=driving"
+        return route_url
+    else:
+        return None
+
+#Grabs user input for destination, and gets the route URL
+destination = input("Enter your destination: ")
+
+route_url = get_directions_to_location(grab_user_location(), destination)
+if route_url:
+    print(f"Opening route: {route_url}")
+else:
+    print("Could not fetch directions.")
