744a855dd0d191c6ee66cfd01cb6a5583ebf404c
jonaht04 <jonaht@udel.edu>
2025-03-02 00:33:24 -0500

    reformatted files to typescript; google maps api for react only describes typescript implementations errors only show up in console log on browser

--- google_api.jsx
+++ google_api.jsx
@@ -1,11 +1,2 @@
-//test location
-const position = {lat: 39.744432, long: -75.545100};
-//@ts-ignore
-const {Map} = await google.maps.importLibrary("maps");
-const { Library } = await google.maps.importLibrary("marker");
-//generate map
-map = new Map(document.getElementById("map"),{
-    zoom: 4, center: position, MapId: "DEMO_MAP_ID",}
-);
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 // index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
-import App from './App';
+import App from './App.tsx';
 import './App.css';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot.
 
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
