e23dfe47ccd18ff89a82136bd72e7fe26adc8c33
Axel Rodriguez-Leon <axelarl@udel.edu>
2025-03-02 02:27:59 -0500

    Google Maps now available

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // App.jsx
 import React from 'react';
 import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import './App.css';
 
 function App() {
   return (
@@ -9,14 +10,15 @@ function App() {
       <header className="App-header">
         Axel, Haiya, Jonah, Shaina HenHacks Team Project
       </header>
-      <APIProvider apiKey={'INSERT API KEY HERE'} onLoad={() => console.log('Maps API has loaded.')}>
+      <APIProvider apiKey={'API Key Here'} onLoad={() => console.log('Maps API has loaded.')}>
       <h1>Hello, world!</h1>
       <Map
         defaultZoom={13}
         defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
         onCameraChanged={ (ev: MapCameraChangedEvent) =>
           console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-        }>
+        }
+        className = "Map">
       </Map>
       </APIProvider>
       <p>Growlithe</p>
