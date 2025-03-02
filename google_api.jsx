cd331334e6ced5555473094161c8876352b0716c
jonaht04 <jonaht@udel.edu>
2025-03-01 22:34:40 -0500

    finished writing test; proceeding with error checking

--- google_api.jsx
+++ google_api.jsx
@@ -2,4 +2,10 @@
 const position = {lat: 39.744432, long: -75.545100};
 //@ts-ignore
 const {Map} = await google.maps.importLibrary("maps");
+const { Library } = await google.maps.importLibrary("marker");
+//generate map
+map = new Map(document.getElementById("map"),{
+    zoom: 4, center: position, MapId: "DEMO_MAP_ID",}
+);
+
 
